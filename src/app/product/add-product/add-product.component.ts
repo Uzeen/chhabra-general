c5610e89768b524db8f5faea999357fc5dd653c4
Uzeen Chhabra <uzeen619@gmail.com>
2021-06-17 13:06:39 +0530

    New Scanner

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ProductService } from '../product.service';
 import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
 import { AlertController } from '@ionic/angular';
-import { BarcodeScannerLivestreamComponent } from "ngx-barcode-scanner";
+import Quagga from 'quagga';
+
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.scss'],
 })
-export class AddProductComponent implements OnInit {
+export class AddProductComponent implements OnInit,AfterViewInit {
 
   productForm = this.formBuilder.group({
     Name : new FormControl(null),
@@ -38,24 +39,40 @@ export class AddProductComponent implements OnInit {
     this.presentAlert(this.productForm.value.Name);
   }
 
-  @ViewChild(BarcodeScannerLivestreamComponent)
-  barcodeScanner: BarcodeScannerLivestreamComponent;
- 
-  barcodeValue;
- 
-  scan(){
-    this.barcodeScanner.start();
-    alert("Starting");
+  scan(data){
+    alert(data);
   }
- 
-  onValueChanges(result) {
-    this.barcodeValue = result.codeResult.code;
-    alert(result.codeResult.code);
+
+  errorMessage: string;
+  ngAfterViewInit(): void {
+    Quagga.init({
+      inputStream: {
+        constraints: {
+          facingMode: 'environment' // restrict camera type
+        },
+        area: { // defines rectangle of the detection
+          top: '40%',    // top offset
+          right: '0%',  // right offset
+          left: '0%',   // left offset
+          bottom: '40%'  // bottom offset
+        },
+      },
+      decoder: {
+        readers: ['ean_reader'] // restrict code types
+      },
+    },
+    (err) => {
+      if (err) {
+        this.errorMessage = `QuaggaJS could not be initialized, err: ${err}`;
+      } else {
+        Quagga.start();
+        Quagga.onDetected((res) => {
+          window.alert(`code: ${res.codeResult.code}`);
+        })
+      }
+    });  
+  }
+
     
-  }
- 
-  onStarted(started) {
-    console.log(started);
-  }
 
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -6,13 +6,11 @@ import { ProductPageRoutingModule } from './product-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProductPage } from './product.page';
 import { AddProductComponent } from './add-product/add-product.component';
-import { BarcodeScannerLivestreamModule } from "ngx-barcode-scanner";
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    BarcodeScannerLivestreamModule,
     IonicModule,
     ProductPageRoutingModule
   ],
